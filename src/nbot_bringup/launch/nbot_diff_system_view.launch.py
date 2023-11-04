5d45feeb2a4c9e4c786e4f893a939cfaebb17812
Nguyen Van Nguyen <ngvan.nguyen164@gmail.com>
2023-11-04 16:12:59 +0700

    hardware finished

--- src/nbot_bringup/launch/nbot_diff_system_view.launch.py
+++ src/nbot_bringup/launch/nbot_diff_system_view.launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
     )
     robot_description = {"robot_description": robot_description_content}
 
-    robot_controllers = PathJoinSubstitution(
+    robot_controllers_param = PathJoinSubstitution(
         [
             FindPackageShare("nbot_bringup"),
             "config",
@@ -32,14 +32,22 @@ def generate_launch_description():
         ]
     )
 
+    robot_localization_param = PathJoinSubstitution(
+        [
+            FindPackageShare("nbot_bringup"),
+            "config",
+            "ekf.yaml",
+        ]
+    )
+
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description, robot_controllers],
+        parameters=[robot_description, robot_controllers_param],
         remappings=[
             # ("/imu_sensor_node/imu", "/_imu/data_raw"),
             # ("~/motors_cmd", "/_motors_cmd"),
-            # ("~/motors_response", "/_motors_response"),
+            ("/odom", "/wheels/odom"),
             ("/nbot_diff_base_controller/cmd_vel_unstamped", "/cmd_vel"),
         ],
     )
@@ -47,7 +55,7 @@ def generate_launch_description():
     robot_state_pub_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        output="both",
+        output="screen",
         parameters=[robot_description],
     )
 
@@ -94,6 +102,7 @@ def generate_launch_description():
                                 0.044890057238382707, 1.2981683205953654, -0.1173361838042438, 
                                 0.007231924972024633, -0.11733618380424381, 0.7835617468652673]}  
             ],
+        remappings=[("/imu","/imu/data")]
     )
 
     scan_node =  Node(
@@ -108,22 +117,20 @@ def generate_launch_description():
         }]
     )
 
-    # imu_broadcaster_spawner = Node(
-    #     package="controller_manager",
-    #     executable="spawner.py",
-    #     arguments=[
-    #         "nbot_imu_broadcaster",
-    #         "--controller-manager",
-    #         "/controller_manager",
-    #     ],
-    # )
+    # robot_localization_node = Node(
+    #     package='robot_localization',
+    #     executable='ekf_node',
+    #     name='ekf_filter_node',
+    #     output='screen',
+    #     parameters=[robot_localization_param],
+    # ),
 
     # # Delay start of imu_broadcaster after robot_controller
     # # when spawning without delay ros2_control_node sometimes crashed
-    # delay_imu_broadcaster_spawner_after_robot_controller_spawner = RegisterEventHandler(
+    # delay_robot_localization_after_robot_controller_spawner = RegisterEventHandler(
     #     event_handler=OnProcessExit(
-    #         target_action=robot_controller_spawner,
-    #         on_exit=[imu_broadcaster_spawner],
+    #         target_action=robot_localization_node,
+    #         on_exit=[robot_controller_spawner],
     #     )
     # )
 
@@ -133,8 +140,9 @@ def generate_launch_description():
         joint_state_broadcaster_spawner,
         delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
         imu_node,
-        scan_node
-        # delay_imu_broadcaster_spawner_after_robot_controller_spawner,
+        scan_node,
+        # delay_robot_localization_after_robot_controller_spawner,
+        # robot_localization_node
     ]
 
     return LaunchDescription(actions)
